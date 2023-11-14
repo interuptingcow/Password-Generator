91bd6976db17f84d2a109dd64ee920d70ca8b94f
interuptingcow <npad28@gmail.com>
2023-11-13 18:32:40 -0800

    added JS prompts

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
+  var passwordLength = prompt("How long would you like your password to be? Choose between 8 and 128");
+  var passwordLowerCase = confirm("Would you like to include lowercase letters?");
+  var passwordUpperCase = confirm("Would you like to include uppercase letters?");
+  var passwordNumbers = confirm("Would you like to include numbers?")
+  var passwordSpecialCharacters = confirm("Would you like to include special characters?");
+  
 
 }
 
